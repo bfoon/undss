10b3544ddef898653aa8488f8b96323ff580e99e
babou <baboucarrfoon@gmail.com>
2025-10-22 04:01:34 +0000

    undss16

--- un_security_system/un_security_system/settings.py
+++ un_security_system/un_security_system/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wouhxk0li%q9hdy#d$)@ldsz#=36dfj)$%$*=r_wnosa!o-@a*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
